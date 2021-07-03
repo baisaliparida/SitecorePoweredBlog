73bff25a4ea1b669d98679c4522c7e34714ac244
baisaliparida <71024348+baisaliparida@users.noreply.github.com>
2021-07-03 13:05:24 +0530

    change

--- src/Feature/Postcard/code/Controllers/PostcardController.cs
+++ src/Feature/Postcard/code/Controllers/PostcardController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using Sitecore.Web.UI.WebControls;
+using SitecorePoweredBlog.Feature.Postcard.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,77 @@ namespace SitecorePoweredBlog.Feature.Postcard.Controllers
     public class PostcardController : Controller
     {
         // GET: Postcard
-        public ActionResult Index()
+        
+        public ActionResult Postcard()
         {
-            return View();
+
+            var result = new List<Article>();
+            try
+            {
+                Sitecore.Data.Database database = Sitecore.Configuration.Factory.GetDatabase("web");
+                Sitecore.Data.Items.Item[] allItems = database.SelectItems(@"fast:/sitecore/content/SitecorePoweredBlog/Article//*[@@templateid='{E1F2D433-B9FE-4684-B987-2D0E270A53C7}']");
+
+                foreach (var item in allItems)
+                {
+                    Sitecore.Data.Fields.ReferenceField dl = item.Fields["Category"];
+                    string str = dl != null && dl.TargetItem != null ? dl.TargetItem.Name : "";
+                    result.Add(new Article
+                    {
+                        Id = FieldRenderer.Render(item, "Id"),
+                        Title = FieldRenderer.Render(item, "Title"),
+                        ShortDescription = FieldRenderer.Render(item, "ShortDescription"),
+                        ShortImage = FieldRenderer.Render(item, "ShortImage"),
+                        PostedDate = Convert.ToDateTime(FieldRenderer.Render(item, "PostedDate")),
+                        Author = FieldRenderer.Render(item, "Author"),
+                        Category = str//FieldRenderer.Render(item, "Category"),
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            //var items = Sitecore.Context.Database.SelectItems("{56741DF6-7290-4E29-B0D1-11A05EE63AFF}");
+
+            return View("~/Views/SitecorePoweredBlog/Postcard.cshtml", result);
+        }
+
+        public ActionResult blogdetail(string blogId)
+        {
+            ViewBag.blogId = blogId;
+            var result = new Article();
+            try
+            {
+
+                var item = Sitecore.Context.Database.GetItem(blogId);
+                Sitecore.Data.Fields.ReferenceField dl = item.Fields["Category"];
+                string str = dl != null && dl.TargetItem != null ? dl.TargetItem.Name : "";
+                List<string> tags = new List<string>();
+                Sitecore.Data.Fields.MultilistField multilistField = item.Fields["Tags"];
+                if (multilistField != null)
+                {
+                    foreach (Sitecore.Data.Items.Item tag in multilistField.GetItems())
+                    {
+                        tags.Add(tag.Name);
+                    }
+                }
+                result = new Article
+                {
+                    Id = FieldRenderer.Render(item, "Id"),
+                    Title = FieldRenderer.Render(item, "Title"),
+                    LongDescription = FieldRenderer.Render(item, "LargeDescription"),
+                    LargeImage = FieldRenderer.Render(item, "LargeImage"),
+                    PostedDate = Convert.ToDateTime(FieldRenderer.Render(item, "PostedDate")),
+                    Author = FieldRenderer.Render(item, "Author"),
+                    Category = str,//FieldRenderer.Render(item, "Category"),
+                    Tags = tags
+                };
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return View("~/Views/SitecorePoweredBlog/blog-detail.cshtml", result);
         }
     }
 }
